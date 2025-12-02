c0d1e6a49ae8b2d14be0196425597b72896a51be
ramiabdelghafour <itmeabdelghafour@gmail.com>
2025-12-02 18:47:39 +0100

    complete modal form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-// modal
+// ============= modal =============
 const addWorkerBtn = document.getElementById("addWorker");
 const modal = document.getElementById("employee-modal");
 const closeModalBtn = document.getElementById("close-modal");
 
-// open modal
+// open modale
 addWorkerBtn.addEventListener("click", () => {
   modal.classList.remove("hidden");
 });
@@ -11,14 +11,12 @@ addWorkerBtn.addEventListener("click", () => {
 // close modal
 closeModalBtn.addEventListener("click", () => {
   modal.classList.add("hidden");
-  document.body.classList.remove("overflow-hidden");
 });
 
-// hide the modale
-modal.addEventListener("click", (e) => {
+// hide modal
+modal.addEventListener("click", e => {
   if (e.target === modal) {
     modal.classList.add("hidden");
-    document.body.classList.remove("overflow-hidden");
   }
 });
 
